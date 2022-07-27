0048d10a45894c656f2cc50c09055a6f2d30ebaf
Masa Shabib <masashabib@yahoo.com>
2022-07-27 11:33:45 +0300

    .

--- task1/src/views/Main.js
+++ task1/src/views/Main.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import UserList from '../components/UserList';
-import NavBar from '../components/NavBar';
 
 export default (props) => {
     const [allUsers, setAllUsers] = useState([]);
